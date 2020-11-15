58b0f1cd4dd32d456ccd4052623a0c13ea446240
lldpwr <lldpwr@gmail.com>
2020-11-15 01:13:47 -0500

    ajout de tous les admin site register

--- hereTo/schedular1/employee/admin.py
+++ hereTo/schedular1/employee/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Personnels
+from .models import Availability
+from .models import TeacherPreferences
+
+admin.site.register(Personnels)
+admin.site.register(Availability)
+admin.site.register(TeacherPreferences)
--- hereTo/schedular1/scheduler/admin.py
+++ hereTo/schedular1/scheduler/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Time
+from .models import Semesters
+from .models import Schedule
+from .models import Logs
+from .models import AdminComments
+
+admin.site.register(Time)
+admin.site.register(Semesters)
+admin.site.register(Schedule)
+admin.site.register(Logs)
+admin.site.register(AdminComments)
